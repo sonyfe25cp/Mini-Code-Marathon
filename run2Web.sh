ae5d010ea68e631151f5c16dac76e05c958f8a1d
sonyfe25cp <sonyfe25cp@gmail.com>
2015-10-30 22:40:20 +0800

    update docker run

--- run2Web.sh
+++ run2Web.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+#0. 命令检测
+#check the docker command
+command -v docker>/dev/null 2>&1 || { echo >&2 "Docker is not installed in this machine"; exit 1; }
+
+command -v mvn>/dev/null 2>&1 || { echo >&2 "Maven is not installed in this machine"; exit 1; }
+
+#1. 编译项目
+(cd hackathon-web && mvn clean package )
+
+webpath="hackathon-web/target/hackathon-web-0.1"
+#2. 检查target下是否有正确的jar包
+if [ -d $webpath ]; then
+  echo "web项目打包成功"
+else
+  echo "web端没有打包成功, 请检查mvn的结果"
+  exit
+fi
+
+abspath=`pwd`/$webpath
+
 for (( i=1; i<3; i++)); do
-  #echo 808$i
-  docker run -p 808$i:8080 -v /Users/omar/workspace/Mini-Code-Marathon/hackathon-web/target/hackathon-web-0.1:/usr/local/tomcat/webapps/ROOT -d docker.cn/docker/tomcat:8-jre8
+  docker run -p 808$i:8080 -v $abspath:/usr/local/tomcat/webapps/ROOT -d docker.cn/docker/tomcat:8-jre8
+  echo "启动tomcat，端口：808"$i
 done
 
 
